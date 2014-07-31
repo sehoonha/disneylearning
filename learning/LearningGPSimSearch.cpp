e94da5e960945ebcf0f9de1535700a7e709beef7
Sehoon Ha <sehoon.ha@gmail.com>
2014-07-31 14:00:59 -0400

    more tests

--- learning/LearningGPSimSearch.cpp
+++ learning/LearningGPSimSearch.cpp
@@ -531,17 +531,17 @@ void worker(LearningGPSimSearchImp* imp) {
         LOG(INFO) << "result: " << v;
 
         finalRunInSimulation(imp);
-        // // Now learn the new dynamics
-        // imp->collectSim0Data();
-        // imp->learnDynamicsInSim1();
+        // Now learn the new dynamics
+        imp->collectSim0Data();
+        imp->learnDynamicsInSim1();
 
         // Export some intermediate data 
         exportIntermediateResult(imp, loop);
 
-        // if (v < imp->goodValue) {
-        //     LOG(INFO) << "termintate. " << v << " is less than threshold " << imp->goodValue;
-        //     break;
-        // }
+        if (v < imp->goodValue) {
+            LOG(INFO) << "termintate. " << v << " is less than threshold " << imp->goodValue;
+            break;
+        }
         if (loop + 1 == imp->maxOuterLoop) {
             LOG(INFO) << "termintate. " << v << " because reach the last iteration";
             break;
